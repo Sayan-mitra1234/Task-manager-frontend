27b073e4381186fff3977b92d8ff10a437041a45
Sayan <sayanmitra132@gmail.com>
2023-12-03 17:42:42 +0530

    the frontend file added

---
+++ src/Component/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+function Home(){
+    return(
+        <div className="task">
+           <p>WELCOME TO TASK MANAGER APP</p>
+        </div>
+    )
+}
+
+export default Home
